831df08ad54200dd8f6eaf81b4ee1d0c256fab92
George Arnold <george.arnold.88@gmail.com>
2020-04-20 10:09:09 -0400

    setting up signin

--- src/signin/signin-router.js
+++ src/signin/signin-router.js
@@ -1,48 +1,48 @@
-const express = require("express");
+const express = require('express');
 const signinRouter = express.Router();
 const bodyParser = express.json();
-const bcrypt = require("bcrypt-nodejs");
+const bcrypt = require('bcrypt-nodejs');
 
-signinRouter.route("/signin").post(bodyParser, (req, res) => {
+signinRouter.route('/signin').post(bodyParser, (req, res) => {
   req.app
-    .get("db")
-    .select("email", "hash")
-    .from("login")
-    .where("email", "=", req.body.email)
-    .then((data) => {
+    .get('db')
+    .select('email', 'hash')
+    .from('login')
+    .where('email', '=', req.body.email)
+    .then(data => {
       const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
       if (isValid) {
         return req.app
-          .get("db")
-          .select("*")
-          .from("users")
-          .where("email", "=", req.body.email)
-          .then((user) => {
+          .get('db')
+          .select('*')
+          .from('users')
+          .where('email', '=', req.body.email)
+          .then(user => {
             res.json(user[0]);
           })
-          .catch((err) => res.status(400).json("unable to get user"));
+          .catch(err => res.status(400).json('unable to get user'));
       } else {
-        res.status(400).json({ password: "Password is incorrect" });
+        res.status(400).json({ password: 'Password is incorrect' });
       }
     })
-    .catch((err) => res.status(400).json({ email: "Email not found" }));
+    .catch(err => res.status(400).json({ email: 'Email not found' }));
 });
-signinRouter.route("/register").post(bodyParser, (req, res) => {
+signinRouter.route('/register').post(bodyParser, (req, res) => {
   const { email, name, password } = req.body;
   const hash = bcrypt.hashSync(password);
-  req.app.get("db").transaction((trx) => {
+  req.app.get('db').transaction(trx => {
     trx
       .insert({
         hash: hash,
-        email: email,
+        email: email
       })
-      .into("login")
-      .returning("email")
-      .then((loginEmail) => {
-        return trx("users")
-          .returning("*")
+      .into('login')
+      .returning('email')
+      .then(loginEmail => {
+        return trx('users')
+          .returning('*')
           .insert({ email: loginEmail[0], name: name, joined: new Date() })
-          .then((user) => {
+          .then(user => {
             res.json(user[0]);
           });
       })
