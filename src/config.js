5934d797056b59ee5e83559ff0fb4449478079df
George Arnold <george.arnold.88@gmail.com>
2020-04-22 16:43:37 -0400

    updating db

--- src/config.js
+++ src/config.js
@@ -1,6 +1,8 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
-  DATABASE_URL: process.env.DATABASE_URL,
-  JWT_SECRET: process.env.JWT_SECRET,
+  DATABASE_URL:
+    process.env.DATABASE_URL ||
+    "postgresql://dunder_mifflin@localhost/dungeoneer-toolkit",
+  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL,
 };
